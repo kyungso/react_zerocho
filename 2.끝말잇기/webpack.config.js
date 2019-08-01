b812fc6e59c779c23364869285bbf5967eeb019b
kyungso <cks678928@gmail.com>
2019-08-01 15:33:00 +0900

    2. 끝말잇기 - class로 컴포넌트 생성 & webpack-hot-loader와 webpack-dev-server로 자동으로 빌드하기

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,30 +1,50 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    name: 'wordrelay-settings',
-    mode: 'development', //실서비스: production
-    devtool: 'eval', //실서비스: hidden-source-map
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+  name: "wordrelay-settings",
+  mode: "development", //실서비스: production
+  devtool: "eval", //실서비스: hidden-source-map
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
 
-    //입력
-    entry: {
-        app: ['./client'],
-    },
-    module:{
-        rules:[{
-            test: /\.jsx?/,
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env','@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            },
-        }],
-    },
-    //출력
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    },
+  //입력
+  entry: {
+    app: ["./client"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR"]
+                },
+                debug: true
+              }
+            ],
+            "@babel/preset-react"
+          ],
+          plugins: [
+              "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel"]
+        }
+      }
+    ]
+  },
+
+  plugins: [],
+
+  //출력
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: '/dist/'    //localhost:8081/dist/app.js 파일 생성 (브라우저에서 확인하기)
+  }
+
 };
