d3f834ca8822b19593bdcdf9e2df3fc61fa5a155
kyungso <cks678928@gmail.com>
2019-08-06 16:57:01 +0900

    add 7.틱택톡 using useReducer(dispatch, action, reducer)

---
+++ 7.틱택토/Tr.jsx
@@ -0,0 +1,21 @@
+import React, { memo, useEffect, useRef } from 'react';
+import Td from './Td';
+
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
+    console.log('tr rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowData === ref.current[0], rowIndex === ref.current[1], dispatch === ref.current[2]);
+        ref.current = [rowData, rowIndex, dispatch];
+    }, [rowData, rowIndex, dispatch]);
+    return (
+        <tr>
+            {Array(rowData.length).fill().map((td, i) => (
+                <Td key={i} cellData={rowData[i]} rowIndex={rowIndex} cellIndex={i} dispatch={dispatch}>{''}</Td>))
+            }
+        </tr>
+    );
+});
+
+export default Tr;
